653a5615981198c0a40ffa0a2754422ff115c6b2
KhoiDOO <khoido8899@gmail.com>
2023-09-28 11:12:43 +0700

    update explore breakout

---
+++ explore/breakout.py
@@ -0,0 +1,21 @@
+import gymnasium as gym
+
+
+env = gym.make("BreakoutNoFrameskip-v4", render_mode="human")
+observation, info = env.reset(seed=42)
+
+for idx in range(1000):
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
+    
+    if idx == 0:
+        print(observation.shape)
+        print(reward)
+        print(info)
+    
+    if terminated or truncated:
+        print(terminated)
+        print(truncated)
+        observation, info = env.reset()
+    
+env.close()
